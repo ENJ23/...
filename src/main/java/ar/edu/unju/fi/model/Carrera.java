6144a75204a634e1c1dcaf548bbdfb3cfd202ab9
Usuario <Usuario@DESKTOP-UAQ2Q4U>
2024-06-13 15:17:32 -0300

    Model Carrera - Jarma Elias Nicolas

--- src/main/java/ar/edu/unju/fi/model/Carrera.java
+++ src/main/java/ar/edu/unju/fi/model/Carrera.java
@@ -1,5 +1,12 @@
 package ar.edu.unju.fi.model;
 
-public class Carrera {
+import lombok.Data;
 
+@Data
+public class Carrera {
+	private String codigo;
+    private String nombre;
+    private Integer cantidadAnios;
+    private Boolean estado;
+    
 }
