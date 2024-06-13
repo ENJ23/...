bcd9a4b4b97e9cf74786e1ffc3eff5a8f1631cec
Usuario <Usuario@DESKTOP-UAQ2Q4U>
2024-06-13 17:01:11 -0300

    Proceso Carrera - Jarma Elias Nicolas

---
+++ src/main/java/ar/edu/unju/fi/service/CarreraService.java
@@ -0,0 +1,12 @@
+package ar.edu.unju.fi.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import ar.edu.unju.fi.model.Carrera;
+@Service
+public interface CarreraService {
+	public void guardarCarrera(Carrera carrera);
+	public List<Carrera> mostrarCarreras();
+}
