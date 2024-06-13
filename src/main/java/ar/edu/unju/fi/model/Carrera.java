6426528a844370bfa9aeee6ad1a3d2d841b57cd3
Usuario <Usuario@DESKTOP-UAQ2Q4U>
2024-06-13 15:28:04 -0300

    Proceso de Carrera - Jarma Elias Nicolas

--- src/main/java/ar/edu/unju/fi/model/Carrera.java
+++ src/main/java/ar/edu/unju/fi/model/Carrera.java
@@ -1,8 +1,11 @@
 package ar.edu.unju.fi.model;
 
+import org.springframework.stereotype.Component;
+
 import lombok.Data;
 
 @Data
+@Component
 public class Carrera {
 	private String codigo;
     private String nombre;
