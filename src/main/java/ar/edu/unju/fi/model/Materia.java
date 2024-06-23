9f21caf7697b7d5bb776e62d1ca87e9d0c267624
PC <PC@MATIAS>
2024-06-23 17:23:36 -0300

    Materia Controller y Service Matias Brotons

--- src/main/java/ar/edu/unju/fi/model/Materia.java
+++ src/main/java/ar/edu/unju/fi/model/Materia.java
@@ -1,8 +1,13 @@
 package ar.edu.unju.fi.model;
 
+import org.springframework.stereotype.Component;
+
+import jakarta.persistence.Entity;
 import lombok.Data;
 
 @Data
+@Component
+@Entity
 public class Materia {
     private String codigo;
     private String nombre;
