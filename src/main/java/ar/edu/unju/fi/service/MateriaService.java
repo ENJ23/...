e20cacf41509a7872c136a948cce4c53c8ec80a7
PC <PC@MATIAS>
2024-06-23 22:39:34 -0300

    Progreso Materia Matias Brotons

--- src/main/java/ar/edu/unju/fi/service/MateriaService.java
+++ src/main/java/ar/edu/unju/fi/service/MateriaService.java
@@ -2,9 +2,18 @@ package ar.edu.unju.fi.service;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import ar.edu.unju.fi.DTO.MateriaDTO;
 import ar.edu.unju.fi.model.Materia;
 
+@Service
 public interface MateriaService {
-	public void guardarMateria(Materia materia);
-	public List<Materia> mostrarMaterias();
+	
+	public void guardarMateria(MateriaDTO materiaDTO);
+	public List<Materia> mostrarMateria();
+	public void borrarMateria(String codigo);
+	public void modificarMateria(MateriaDTO materiaModificada);
+	public MateriaDTO buscarMateria(String codigo);
+
 }
